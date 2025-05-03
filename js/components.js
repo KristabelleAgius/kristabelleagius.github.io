494e849929a202e4543d4af8da8d88258b0611ee
KristabelleAgius <kristabelle.agius.22@um.edu.mt>
2025-05-03 21:31:49 +0200

    c

--- js/components.js
+++ js/components.js
@@ -13,13 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
 function loadComponent(componentName, targetElement) {
     const components = {
         navbar: `
+
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.html">KristabelleAgius</a>
+                <a class="navbar-brand navbar-title" href="index.html">KristabelleAgius</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
